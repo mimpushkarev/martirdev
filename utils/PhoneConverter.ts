17f4d40cbd25c5f8533ed63f3d7e633548e61a2d
AnnaErs <annaershova02@gamil.com>
2023-02-10 17:36:18 +0500

    feat: BIKDARTEM-46: [FRONT] Создать секцию контактов

---
+++ utils/PhoneConverter.ts
@@ -0,0 +1,9 @@
+export const PhoneConverter = (phoneNumberString: string) => {
+    var cleaned = (phoneNumberString).replace(/\D/g, '');
+    var phoneMatchesMask = cleaned.match(/^([8|+7])?(\d{3})(\d{3})(\d{2})(\d{2})$/);
+    if (phoneMatchesMask) {
+      var intlCode = (phoneMatchesMask[1] ? '+7 ' : '');
+      return `${intlCode}(${phoneMatchesMask[2]}) ${phoneMatchesMask[3]}-${phoneMatchesMask[4]}-${phoneMatchesMask[5]}`;
+    }
+    return '';
+  }
